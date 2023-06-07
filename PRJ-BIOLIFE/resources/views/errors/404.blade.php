a312ef23431c5b7b94743eeddaa069c0d17012f4
Manhnd10803 <118704220+Manhnd10803@users.noreply.github.com>
2023-06-07 19:25:28 +0700

    page 404 error

---
+++ PRJ-BIOLIFE/resources/views/errors/404.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('content')
+    <!-- Main content -->
+    <div class="page-contain page-404">
+
+        <div id="main-content" class="main-content">
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="content-404">
+                        <h1 class="heading">404</h1>
+                        <h2 class="title">Oops! That page can't be found.</h2>
+                        <p>Sorry, but the page you are looking for is not found. Please, make sure you have typed the current URL.</p>
+                        <a class="button" href="/">Go to Home</a>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+@endsection
